12a11efd50ac94898b831f00d643513fe655ba8e
SyedSaudAli-gh <saud.saleem93@gmail.com>
2025-02-08 19:16:15 +0500

    update new

---
+++ src/sentry.config.js
@@ -0,0 +1,7 @@
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const moduleExports = {};
+const SentryWebpackPluginOptions = {
+  silent: true,
+};
+module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
